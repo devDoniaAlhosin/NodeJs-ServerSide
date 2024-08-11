7cf5cd1b9b5d7a3eb9124f6aaaaf5db872cb8cc9
nouran <nouranessam782@gmail.com>
2024-08-11 23:40:52 +0300

    test

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-// var express=require('express');
-// var app=express();
-// var bodyParser=require('body-parser');
-// var mongoose=require('mongoose');
-
-// //connect to Mongoose
-// mongoose.connect('mongodb://localhost/bookstore');
-// var db=mongoose.connection
-
-// app.get('/',function(req,res){
-// res.send('hello nodemon ');
-// });
-
-// app.listen(3000)
-// console.log('running on port 3000')
-
 var express = require('express');
 var bodyParser = require('body-parser');
-var db = require('./config/db');  // Import the database connection
-var bookRoutes = require('./routes/bookroutes'); // Import the routes
+const bookController=require('../controllers/booksController')
+// var db = require('./config/db');  // Import the database connection
+// var bookRoutes = require('./routes/bookroutes'); // Import the routes
 
 var app = express();
 
 app.use(bodyParser.json());
 
 // Use the routes
-app.use('/', bookRoutes);
+// app.use('/', bookRoutes);
+
 
 // Start the server
 app.listen(3000, function() {
--- routes/bookroutes.js
+++ routes/bookroutes.js
@@ -1,7 +1,7 @@
 // routes/bookRoutes.js
 var express = require('express');
 var router = express.Router();
-
+// const books
 // Define your route here
 router.get('/', function(req, res) {
   res.send('hello nodemon');
